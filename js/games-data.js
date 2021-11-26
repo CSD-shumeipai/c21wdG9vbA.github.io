883584456d9433809f37521ae23899f15aefbabe
cfengnet <56508911+CSD-shumeipai@users.noreply.github.com>
2021-11-26 18:39:34 +0800

    ss

---
+++ js/games-data.js
@@ -0,0 +1,14 @@
+var gamesData = [{
+	
+	"url": "https://html5.gamedistribution.com/93a8298746454e9f8003c1b83c78cd0a/",
+	"imgSrc": "https://games.infinitynewtab.com/games-img/93a8298746454e9f8003c1b83c78cd0a.jpg",
+	"name": "TANKI ONLINE",
+	"tags": ["ACTION","TANKS","ARCADE","SHOOTER","MULITPLAYER"],
+	"id": "93a8298746454e9f8003c1b83c78cd0a"
+}, {
+	"url": "https://html5.GameDistribution.com/d1eeb899810e4c359805ad860e405847/",
+	"imgSrc": "https://games.infinitynewtab.com/games-img/d1eeb899810e4c359805ad860e405847.jpg",
+	"name": "NITRODRIFT.IO",
+	"tags": ["Multiplayer"],
+	"id": "d1eeb899810e4c359805ad860e405847"
+}]
