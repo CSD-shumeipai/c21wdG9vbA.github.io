7efe671538f386e3f3d08232249eb160f1d653bc
cfengnet <56508911+CSD-shumeipai@users.noreply.github.com>
2021-11-26 19:13:00 +0800

    ss

--- js/games-data.js
+++ js/games-data.js
@@ -1,6 +1,6 @@
 var gamesData = [ {
-	"url": "http://toolbox.csdshumeipai.xyz/unlock-music/",
-	"imgSrc": "http://toolbox.csdshumeipai.xyz/imgs/unlock-music.png",
+	"url": "https://toolbox.csdshumeipai.xyz/unlock-music/",
+	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/unlock-music.png",
 	"name": "音频解锁",
 	"tags": ["Music"],
 	"id": "d1eeb899810e4c359805ad860e405847"
