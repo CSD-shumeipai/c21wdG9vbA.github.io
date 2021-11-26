ec07c1b75615cf73082fc2c998dc8b727275050a
cfengnet <56508911+CSD-shumeipai@users.noreply.github.com>
2021-11-26 21:44:43 +0800

    ss

--- js/games-data.js
+++ js/games-data.js
@@ -4,4 +4,10 @@ var gamesData = [ {
 	"name": "音频解锁",
 	"tags": ["Music"],
 	"id": "d1eeb899810e4c359805ad860e405847"
-},]
+},{
+	"url": "https://toolbox.csdshumeipai.xyz/shou/",
+	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/shou.png",
+	"name": "兽音译者",
+	"tags": ["文字处理"],
+	"id": "d1eeb899810e4c359805ad860e405847"
+}]
